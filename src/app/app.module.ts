3c0bb852851a2c0d370b2ee423663c25c14a9b96
Kristin <kristin.jue@gmail.com>
2021-07-14 10:07:00 -0700

    converted project to use SCSS in addition to CSS, as well as added Angular Material Form Field to recieve input and upon submission, display input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SampleComponent } from './sample/sample.component';
+import { InputComponent } from "./input/input.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from "@angular/material/form-field";
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from "@angular/material/form-field";
 import { DragDropModule } from "@angular/cdk/drag-drop";
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatDatepickerModule} from "@angular/material/datepicker";
+import { MatNativeDateModule} from '@angular/material/core';
 import { CalendarComponent } from "./calendar/calendar.component";
 import { HttpClientModule } from '@angular/common/http';
+import { DemoMaterialModule } from "./material.module";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     CalendarComponent,
-    SampleComponent
+    SampleComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +29,9 @@ import { HttpClientModule } from '@angular/common/http';
     DragDropModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    HttpClientModule
+    HttpClientModule,
+    DemoMaterialModule,
+    FormsModule
   ],
   providers: [
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
