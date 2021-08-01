848250100678dbf497e6cc25c4784715e0492b53
Kristin <kristin.jue@gmail.com>
2021-08-01 12:57:03 -0700

    showing movie title under movie cards again, fixed getMovieEvents(), showing Event data on Home page where hostID == demoID value

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApicallService } from '../apicall.service';
+import { HttpClient } from '@angular/common/http';
+import { MovieEvent } from '../event/event.component';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  movieEvents: MovieEvent[] = [];
+  demoID = "DEMO";
+  constructor(public apicall: ApicallService, private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.loadMovieEvents();
   }
 
+  loadMovieEvents() {
+    return this.apicall.getMovieEvents().subscribe((data) => {
+      console.log(data);
+      for (let index in data) {
+        if (data[index].hostID == this.demoID) {
+          this.movieEvents.push(data[index]);
+        }
+      }
+      console.log(this.movieEvents);
+      })
+  }
 }
