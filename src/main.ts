4d5d028d8f6ddd9f420f000bfa1a1daee7fd54a0
ZenSorcere <zen.sorcere@gmail.com>
2021-08-24 17:56:19 -0700

    failed experiments with cognito and attempted jwt access

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import Amplify from 'aws-amplify';
+import { awsconfig } from './amplify-config';
+import Auth from '@aws-amplify/auth';
+import Storage from '@aws-amplify/storage';
+//import AWSConfig from './aws-exports';
+
+Storage.configure(awsconfig);
+Auth.configure(awsconfig);
+// End Amplify Configuration
+
+Amplify.configure(awsconfig);
 
 if (environment.production) {
   enableProdMode();
