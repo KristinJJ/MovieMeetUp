258c3ea80a9be6c1b72221b3add9f743cf8694a1
ZenSorcere <zen.sorcere@gmail.com>
2021-09-03 15:23:57 -0700

    topnav status works but requires page refresh

--- src/app/topNav/topNav.component.ts
+++ src/app/topNav/topNav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { AuthGuard } from '../auth.guard';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-topNav',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topNav.component.css']
 })
 export class TopNavComponent implements OnInit {
+  public loggedUser = false;
 
-  constructor() { }
+  constructor(private authGuard: AuthGuard, private authService: AuthService) { }
+  checkLoggedInStatus(): boolean {
+    if (sessionStorage.getItem("hostID") !== null) {
+      this.loggedUser = true;
+    }
+    console.log('topnav check: ' + this.loggedUser);
+    return this.loggedUser;
+  }
 
   ngOnInit(): void {
+    this.checkLoggedInStatus();
   }
+  
 
 }
