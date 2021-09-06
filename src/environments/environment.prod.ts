99f83c4bfc5aa3e9a63ee5df2f3ea5ec1fd12f30
KristinJJ <kristin.jue@gmail.com>
2021-09-06 03:08:42 -0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
+  imdbApiKey: "",
+  demoUserID: "DEMO",
   loginURL: "https://moviemeetup.com/home",
   logoutURL: "https://moviemeetup.com/intro"
 };
